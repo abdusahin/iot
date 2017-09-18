414f84c880737ae2f843c52884984ee3d702049a
abdu <abdul.sahin@digital.hmrc.gov.uk>
2017-09-18 07:50:23 +0100

    Added gateway & config

--- create-env.sh
+++ create-env.sh
@@ -1,11 +1,13 @@
 #/bin/bash
 
-docker swarm init --advertise-addr 192.168.99.121
-docker network create --driver overlay --subnet=192.168.0.0/24 overlay-net
+#docker swarm init --advertise-addr 192.168.99.121
+#docker network create --driver overlay --subnet=192.168.0.0/24 overlay-net
 
 #docker volume create -d flocker --name=vol-cassandra1 -o size=1G
 #docker volume create -d flocker --name=vol-cassandra2 -o size=1G
 #docker volume create -d flocker --name=vol-cassandra3 -o size=1G
 
 #docker-compose up -d
+export ADD_SCHEMA_AND_SYSTEM_DATA=true 
+export ADD_DEMO_DATA=true
 docker stack deploy --compose-file=docker-compose.yml iot
